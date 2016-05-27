881e80e5dca905baea6229effb47fc0289676056
evansmike <mike.evans88@gmail.com>
2016-05-26 20:06:53 -0400

    updated process.php and added thanks.php with emailer

--- suggest.php
+++ suggest.php
@@ -5,7 +5,7 @@ $section = "suggest";
 include("inc/header.php"); ?>
 
 <div class="section page">
-    <div class="warpper">
+    <div class="wrapper">
       <h1>Suggest a Media Item</h1>
       <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
       <form method="post" action="process.php">
@@ -26,8 +26,6 @@ include("inc/header.php"); ?>
         <input type="submit" value="Send" />
       </form>
     </div>
-
 </div>
-
 
 <?php include("inc/footer.php"); ?>
